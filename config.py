9be49b75ac71ac178fb0416f44f0df6950418dfe
AragamiSenketsu <99680646+AragamiSenketsu@users.noreply.github.com>
2023-03-06 04:29:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "5742581146:AAHz38p1ktPS2NEaPCBgYeNQZOLYXL-4Vnw"
 DB_CHANNEL_ID = "-1001794831285"
 IS_PRIVATE = false # any input is ok But True preferable
 OWNER_ID = int(5516570253)
-UPDATE_CHANNEL = 
+UPDATE_CHANNEL = ""
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
